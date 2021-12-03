c4ca4a4d3ad3032eacd804f991917d2bde82ca48
Mateusz Charczuk <mateusz.charczuk@op.pl>
2021-12-03 20:04:01 +0100

    galsik

--- lmp9/lmp9/trunk/src/gauss.c
+++ lmp9/lmp9/trunk/src/gauss.c
@@ -1,14 +1,30 @@
 #include "gauss.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
-
+	double ratio;
+	int i, j, k;
+	for(i=0;i<(mat->r);i++)
+	 {
+		if(mat->data[i][i] == 0.0)
+	      	{
+			 printf("Mathematical Error!");
+		 	 return 1;
+		 }
+		 for(j=i+1;j<(mat->c);j++)
+		 {
+		 	ratio = (mat->data[j][i])/(mat->data[i][i]);
+	  
+			for(k=0;k<=(mat->c);k++)
+			 {	
+				 (mat->data[j][k]) = (mat->data[j][k]) - ratio*(mat->data[i][k]);
+			 }
+		}
+	}
 		return 0;
 }
 
