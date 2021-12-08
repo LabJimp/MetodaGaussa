912436d8802c4fe30df1092d87ea0325ca52c9fd
Mateusz Charczuk <mateusz.charczuk@op.pl>
2021-12-08 16:36:44 +0100

    gauss_baza

--- lmp9/lmp9/trunk/src/gauss.c
+++ lmp9/lmp9/trunk/src/gauss.c
@@ -11,21 +11,21 @@ eliminate (Matrix * mat, Matrix * b)
 {
   double ratio;
   double *tmp;
-  int i, j, k, max;
+  int i, j, k, Max;
   for (i = 0; i < mat->r; i++)
     {
-      max = i+1;
+      Max = i+1;
       for (k = i + 1; k < mat->r; k++)
-	  if (fabs(mat->data[k][i]) > fabs(mat->data[max][i]))
-	    max = k;
+	  if (fabs(mat->data[k][i]) > fabs(mat->data[Max][i]))
+	    Max = k;
       if ((max != i + 1) && (i != mat->r - 1))
 	{
 	  tmp = mat->data[i + 1];
-	  mat->data[i + 1] = mat->data[max];
-	  mat->data[max] = tmp;
+	  mat->data[i + 1] = mat->data[Max];
+	  mat->data[Max] = tmp;
 	  tmp = b->data[i+1];
-	  b->data[i+1] = b->data[max];
-	  b->data[max] = tmp;
+	  b->data[i+1] = b->data[Max];
+	  b->data[Max] = tmp;
 	}
       if (mat->data[i][i] == 0.0)
 	{
